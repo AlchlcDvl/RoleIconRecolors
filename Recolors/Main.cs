c5e528182052da538b804e6f621015731c27d6b8
AD <56699741+AlchlcDvl@users.noreply.github.com>
2023-09-03 00:52:08 -0400

    updates

--- Recolors/Main.cs
+++ Recolors/Main.cs
@@ -5,14 +5,3 @@ public class Main
 {
     public void Start() => Console.WriteLine("Time travelling!");
 }
-
-[SalemMenuItem]
-public class MenuItem
-{
-    public static SalemMenuButton menuButtonName = new()
-    {
-        Label = "Role Icon Recolors",
-        Icon = FromResources.LoadSprite("Recolors.Resources.thumbnail.png"),
-        OnClick = () => {}
-    };
-}
