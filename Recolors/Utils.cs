414a3560d00d84a83f94950fb1112b71bc0459e2
AD <56699741+AlchlcDvl@users.noreply.github.com>
2023-10-18 10:31:47 -0400

    rename files and code optimisation

--- Recolors/Utils.cs
+++ Recolors/Utils.cs
@@ -4,9 +4,9 @@ public static class Utils
 {
     public static T? Random<T>(this IEnumerable<T>? input, T? defaultVal = default)
     {
-        var list = input as IList<T> ?? input.ToList();
+        var list = input.ToList();
         return list.Count == 0 ? defaultVal : list[URandom.Range(0, list.Count)];
     }
 
-    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action) => (source as IList<T> ?? source.ToList()).ForEach(action);
+    public static void ForEach<T>(this IEnumerable<T>? source, Action<T>? action) => source?.ToList()?.ForEach(action);
 }
