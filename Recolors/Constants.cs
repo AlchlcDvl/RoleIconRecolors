b95b19487360095f90a03dea8104e13a748d835d
AD <56699741+AlchlcDvl@users.noreply.github.com>
2023-10-30 13:12:18 -0400

    fix settings

--- Recolors/Constants.cs
+++ Recolors/Constants.cs
@@ -2,7 +2,7 @@ namespace Recolors;
 
 public static class Constants
 {
-    public static bool EnableIcons => ModSettings.GetBool("Enable Recolors", "alchlcdvl.recolors");
+    public static bool EnableIcons => ModSettings.GetBool("Enable Custom Icons", "alchlcdvl.recolors");
     public static int EasterEggChance => ModSettings.GetInt("Easter Egg Icon Chance", "alchlcdvl.recolors");
     //public static string CurrentPack => ModSettings.GetString("Selected Icon Pack", "alchlcdvl.recolors");
 }
